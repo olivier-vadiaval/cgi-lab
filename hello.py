d823cd7bda0ec6537c516df5a3aa48907e71774d
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-23 02:00:30 -0400

    Call login_page

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import cgi
 import cgitb
 cgitb.enable()
 
 import os
+from time import gmtime, strftime
 from templates import *
+from secret import *
 
-form = cgi.FieldStorage()
-
-print("Content-Type: text/html")
-print()
+print("Content-Type: text/html\r\n\r\n")
 print("<html>")
 print("<H1>" + os.environ.get("QUERY_STRING") + "</H1>")
 print("<H1>" + os.environ.get("HTTP_USER_AGENT") + "</H1>")
