50d939451f7c1943f4bceec2a4dac1d263333ecd
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-23 02:00:45 -0400

    Set cookies and show secret_page

--- login.py
+++ login.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import cgi
 import cgitb
 cgitb.enable()
 
 import os
+from secret import *
 from templates import *
 
 form = cgi.FieldStorage()
-username = form.getvalue("username")
+user = form.getvalue("username")
 pwd = form.getvalue("password")
 
-print("Content-Type: text/html")
-print()
-print("<html>")
-print("<H1>Username: " + username + "</H1>")
-print("<H1>Password: " + pwd + "</H1>")
-print("</html>")
+cookies = os.environ.get("HTTP_COOKIE").split(";")
+
+if username == user and password == pwd:
+    print("Set-Cookie: username = " + username + ";")
+    print("Set-Cookie: password = " + password + ";")
+
+print("Content-Type: text/html\r\n\r\n")
+print("Logged in")
+
+if len(cookies) == 2:
+    print(secret_page(user, pwd))
